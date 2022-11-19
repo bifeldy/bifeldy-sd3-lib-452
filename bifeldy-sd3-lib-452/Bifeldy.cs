1d94eb29a3303b1a03bb6ba5897ad93ec2f0ca1e
Bifeldy <bifeldy@gmail.com>
2022-11-19 18:29:16 +0900

    Add project files.

---
+++ bifeldy-sd3-lib-452/Bifeldy.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Mail         :: bias@indomaret.co.id
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Bifeldy's Initial Main Application
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bifeldy_sd3_lib_452 {
+
+    public class Bifeldy {
+
+        public Bifeldy() {
+            // 
+        }
+
+    }
+
+}
