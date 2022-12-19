c0ea51782b83d4ee7ca688766faa1ed1fff40111
Bifeldy <bifeldy@gmail.com>
2022-12-19 12:21:36 +0700

    Try Catch ~ Nyerah

--- bifeldy-sd3-lib-452/Utilities/Logger_.cs
+++ bifeldy-sd3-lib-452/Utilities/Logger_.cs
@@ -20,8 +20,9 @@ using System.IO;
 namespace bifeldy_sd3_lib_452.Utilities {
 
     public interface ILogger {
+        string LogInfoFolderPath { get; }
         string LogErrorFolderPath { get; }
-        void SetReportLogInfoTarget(object control);
+        void SetReportLogInfoTarget<T>(IProgress<T> reporter);
         void WriteLog(string subject, string body, bool newLine = false);
         void WriteError(string errorMessage, int skipFrame = 1);
         void WriteError(Exception errorException, int skipFrame = 2);
@@ -31,45 +32,68 @@ namespace bifeldy_sd3_lib_452.Utilities {
 
         private readonly IApplication _app;
 
+        public string LogInfoFolderPath { get; }
+
         public string LogErrorFolderPath { get; }
 
-        public IProgress<string> LogInformation = null;
+        public IProgress<dynamic> LogInformation = null;
 
         public CLogger(IApplication app) {
             _app = app;
 
+            LogInfoFolderPath = Path.Combine(_app.AppLocation, "Info_Logs");
+            if (!Directory.Exists(LogInfoFolderPath)) {
+                Directory.CreateDirectory(LogInfoFolderPath);
+            }
             LogErrorFolderPath = Path.Combine(_app.AppLocation, "Error_Logs");
             if (!Directory.Exists(LogErrorFolderPath)) {
                 Directory.CreateDirectory(LogErrorFolderPath);
             }
         }
 
-        public void SetReportLogInfoTarget(dynamic control) {
-            LogInformation = new Progress<string>(log => {
-                control.Text += log;
-            });
+        public void SetReportLogInfoTarget<T>(IProgress<T> reporter) {
+            LogInformation = (IProgress<dynamic>) reporter;
         }
 
         public void WriteLog(string subject, string body, bool newLine = false) {
-            string content = $"[{DateTime.Now:HH:mm:ss tt zzz}] {subject} :: {body} {Environment.NewLine}";
-            if (newLine) {
-                content += Environment.NewLine;
+            try {
+                string content = $"[{DateTime.Now:HH:mm:ss tt zzz}] {subject} :: {body} {Environment.NewLine}";
+                if (newLine) {
+                    content += Environment.NewLine;
+                }
+                if (LogInformation != null) {
+                    LogInformation.Report(content);
+                }
+                StreamWriter sw = new StreamWriter($"{LogInfoFolderPath}/{DateTime.Now:dd-MM-yyyy}.txt", true);
+                sw.WriteLine(content);
+                sw.Flush();
+                sw.Close();
             }
-            if (LogInformation != null) {
-                LogInformation.Report(content);
+            catch (Exception ex) {
+                WriteError(ex);
             }
         }
 
         public void WriteError(string errorMessage, int skipFrame = 1) {
-            StackFrame fromsub = new StackFrame(skipFrame, false);
-            StreamWriter sw = new StreamWriter($"{LogErrorFolderPath}/{DateTime.Now:dd-MM-yyyy}.txt", true);
-            sw.WriteLine($"##");
-            sw.WriteLine($"#  ErrDate : {DateTime.Now:dd-MM-yyyy HH:mm:ss}");
-            sw.WriteLine($"#  ErrFunc : {fromsub.GetMethod().Name}");
-            sw.WriteLine($"#  ErrInfo : {errorMessage}");
-            sw.WriteLine($"##");
-            sw.Flush();
-            sw.Close();
+            try {
+                StackFrame fromsub = new StackFrame(skipFrame, false);
+                StreamWriter sw = new StreamWriter($"{LogErrorFolderPath}/{DateTime.Now:dd-MM-yyyy}.txt", true);
+                sw.WriteLine($"##");
+                sw.WriteLine($"#  ErrDate : {DateTime.Now:dd-MM-yyyy HH:mm:ss}");
+                sw.WriteLine($"#  ErrFunc : {fromsub.GetMethod().Name}");
+                sw.WriteLine($"#  ErrInfo : {errorMessage}");
+                sw.WriteLine($"##");
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex) {
+                // Nyerah ~~
+                // Re-Throw Lempar Ke Main Thread (?)
+                // Mungkin Akan Membuat Force-Close Program
+                // Comment Jika Mau Beneran Menyerah Dan Gak Ngapa"in
+                // Agar Tidak Force-Close Dan Tetap Jalan
+                throw ex;
+            }
         }
 
         public void WriteError(Exception errorException, int skipFrame = 2) {
