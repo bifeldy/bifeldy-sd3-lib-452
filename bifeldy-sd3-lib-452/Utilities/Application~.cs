04d2de718eb1e0aa3cc7bc344861f2db2fdd7e77
B. Bias A. Ch <bias@indomaret.co.id>
2023-10-24 10:33:37 +0700

    Windows Startup

--- bifeldy-sd3-lib-452/Utilities/Application~.cs
+++ bifeldy-sd3-lib-452/Utilities/Application~.cs
@@ -17,11 +17,9 @@ using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
-using System.Security.AccessControl;
 using System.Windows;
 
 using bifeldy_sd3_lib_452.Models;
-using Microsoft.Win32;
 
 namespace bifeldy_sd3_lib_452.Utilities {
 
@@ -40,7 +38,6 @@ namespace bifeldy_sd3_lib_452.Utilities {
         string[] GetAllIpAddress();
         string[] GetAllMacAddress();
         bool IsUsingPostgres { get; set; }
-        void SetWindowsStartup(bool startAtBoot = false);
     }
 
     public class CApplication : IApplication {
@@ -154,27 +151,6 @@ namespace bifeldy_sd3_lib_452.Utilities {
 
         public string[] GetAllMacAddress() {
             return GetIpMacAddress().Where(d => !string.IsNullOrEmpty(d.MAC_ADDRESS)).Select(d => d.MAC_ADDRESS.ToUpper()).ToArray();
-        }
-
-        public void SetWindowsStartup(bool startAtBoot = false) {
-            string registryKeyPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
-            RegistryHive rh = RegistryHive.CurrentUser;
-            RegistryView rv = Environment.Is64BitProcess ? RegistryView.Registry64 : RegistryView.Registry32;
-            RegistryKeyPermissionCheck rkpc = RegistryKeyPermissionCheck.ReadWriteSubTree;
-            RegistryRights rr = RegistryRights.FullControl;
-            RegistryValueKind rvk = RegistryValueKind.String;
-            using (RegistryKey bk = RegistryKey.OpenBaseKey(rh, rv)) {
-                using (RegistryKey sk = bk.OpenSubKey(registryKeyPath, rkpc, rr)) {
-                    if (sk != null) {
-                        if (sk.GetValue(AppName) != null) {
-                            sk.DeleteValue(AppName, false);
-                        }
-                        if (startAtBoot) {
-                            sk.SetValue(AppName, AppPath, rvk);
-                        }
-                    }
-                }
-            }
         }
 
     }
---
+++ bifeldy-sd3-lib-452/Utilities/WinReg_.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Windows Registry
+ *              :: Harap Didaftarkan Ke DI Container
+ * 
+ */
+
+using System;
+using System.Security.AccessControl;
+
+using Microsoft.Win32;
+
+namespace bifeldy_sd3_lib_452.Utilities {
+
+    public interface IWinReg {
+        void SetWindowsStartup(bool startAtBoot);
+        void ReSetConfigWindowsStartup();
+    }
+
+    public sealed class CWinReg : IWinReg {
+
+        private readonly IApplication _app;
+        private readonly IConfig _config;
+
+        public CWinReg (IApplication app, IConfig config) {
+            _app = app;
+            _config = config;
+        }
+
+        public void SetWindowsStartup(bool startAtBoot) {
+            string registryKeyPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+            RegistryHive rh = RegistryHive.CurrentUser;
+            RegistryView rv = Environment.Is64BitProcess ? RegistryView.Registry64 : RegistryView.Registry32;
+            RegistryKeyPermissionCheck rkpc = RegistryKeyPermissionCheck.ReadWriteSubTree;
+            RegistryRights rr = RegistryRights.FullControl;
+            RegistryValueKind rvk = RegistryValueKind.String;
+            using (RegistryKey bk = RegistryKey.OpenBaseKey(rh, rv)) {
+                using (RegistryKey sk = bk.OpenSubKey(registryKeyPath, rkpc, rr)) {
+                    if (sk != null) {
+                        if (sk.GetValue(_app.AppName) != null) {
+                            sk.DeleteValue(_app.AppName, false);
+                        }
+                        if (startAtBoot) {
+                            sk.SetValue(_app.AppName, _app.AppPath, rvk);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ReSetConfigWindowsStartup() {
+            SetWindowsStartup(_config.Get<bool>("WindowsStartup", bool.Parse(_app.GetConfig("windows_startup"))));
+        }
+
+    }
+
+}
