f162d53193b7c53b202a7d2afce0330ec2665db8
B. Bias A. Ch <bias@indomaret.co.id>
2023-12-27 13:54:50 +0700

    Ekstensi Converter ~

---
+++ bifeldy-sd3-lib-452/Extensions/DataTableExtensions.cs
@@ -0,0 +1,45 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Tidak Untuk Didaftarkan Ke DI Container
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace bifeldy_sd3_lib_452.Extensions {
+
+    public static class DataTableExtensions {
+
+        public static List<T> ToList<T>(this DataTable dt) {
+            List<string> columnNames = dt.Columns.Cast<DataColumn>().Select(c => c.ColumnName.ToUpper()).ToList();
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            return dt.AsEnumerable().Select(row => {
+                T objT = Activator.CreateInstance<T>();
+                foreach (PropertyInfo pro in properties) {
+                    if (columnNames.Contains(pro.Name.ToUpper())) {
+                        try {
+                            pro.SetValue(objT, row[pro.Name]);
+                        }
+                        catch {
+                            //
+                        }
+                    }
+                }
+                return objT;
+            }).ToList();
+        }
+
+    }
+
+}
