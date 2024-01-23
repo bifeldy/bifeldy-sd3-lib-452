d968d4826a842e24866593190684d068bdf10fe3
Bifeldy <bifeldy@gmail.com>
2024-01-24 02:25:57 +0900

    Fix Extension ~

--- bifeldy-sd3-lib-452/Extensions/ByteExtensions.cs
+++ bifeldy-sd3-lib-452/Extensions/ByteExtensions.cs
@@ -11,17 +11,29 @@
 */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace bifeldy_sd3_lib_452.Extensions {
 
     public static class ByteExtensions {
 
-        public static string ToString(this byte[] bytes, bool removeHypens = true) {
+        public static string ToStringHex(this byte[] bytes, bool removeHypens = true) {
             string hex = BitConverter.ToString(bytes);
             if (removeHypens) {
                 return hex.Replace("-", "");
             }
             return hex;
+        }
+
+        public static IEnumerable<byte[]> Split(this byte[] value, int bufferLength) {
+            int countOfArray = value.Length / bufferLength;
+            if (value.Length % bufferLength > 0) {
+                countOfArray++;
+            }
+            for (int i = 0; i < countOfArray; i++) {
+                yield return value.Skip(i * bufferLength).Take(bufferLength).ToArray();
+            }
         }
 
     }
