da4d665e8b8ee3ae2ce6fb42e3a2ef5ea611e6f7
Bifeldy <bifeldy@gmail.com>
2024-01-24 02:27:10 +0900

    Fix Kafka Model ~

--- bifeldy-sd3-lib-452/Models/KafkaModel_.cs
+++ bifeldy-sd3-lib-452/Models/KafkaModel_.cs
@@ -16,10 +16,21 @@ using Confluent.Kafka;
 namespace bifeldy_sd3_lib_452.Models {
 
     public sealed class KafkaMessage<TKey, TValue> : Message<TKey, TValue> {
-        //
+        public new Headers Headers { get; set; }
+        public new TKey Key { get; set; }
+        public new TValue Value { get; set; }
+        public new Timestamp Timestamp { get; set; }
     }
 
     public sealed class KafkaDeliveryResult<TKey, TValue> : DeliveryResult<TKey, TValue> {
+        public new Headers Headers { get; set; }
+        public new string Key { get; set; }
+        public new Message<string, string> Message { get; set; }
+        public new Offset Offset { get; set; }
+        public new Partition Partition { get; set; }
+        public new PersistenceStatus Status { get; set; }
+        public new Timestamp Timestamp { get; set; }
+        public new string Topic { get; set; }
         public new TopicPartition TopicPartition { get; set; }
     }
 
