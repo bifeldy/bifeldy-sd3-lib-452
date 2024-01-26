66a2d1d3e03d7cc1ec4603c894b7a8ab1ba0d4d0
B. Bias A. Ch <bias@indomaret.co.id>
2024-01-26 11:05:02 +0700

    Fix Kafka

--- bifeldy-sd3-lib-452/Models/KafkaModel_.cs
+++ bifeldy-sd3-lib-452/Models/KafkaModel_.cs
@@ -16,22 +16,65 @@ using Confluent.Kafka;
 namespace bifeldy_sd3_lib_452.Models {
 
     public sealed class KafkaMessage<TKey, TValue> : Message<TKey, TValue> {
-        public new Headers Headers { get; set; }
-        public new TKey Key { get; set; }
-        public new TValue Value { get; set; }
-        public new Timestamp Timestamp { get; set; }
+        public new Headers Headers {
+            get => base.Headers;
+            set => base.Headers = value;
+        }
+        public new TKey Key {
+            get => base.Key;
+            set => base.Key = value;
+        }
+        public new TValue Value {
+            get => base.Value;
+            set => base.Value = value;
+        }
+        public new Timestamp Timestamp {
+            get => base.Timestamp;
+            set => base.Timestamp = value;
+        }
     }
 
     public sealed class KafkaDeliveryResult<TKey, TValue> : DeliveryResult<TKey, TValue> {
-        public new Headers Headers { get; set; }
-        public new string Key { get; set; }
-        public new Message<string, string> Message { get; set; }
-        public new Offset Offset { get; set; }
-        public new Partition Partition { get; set; }
-        public new PersistenceStatus Status { get; set; }
-        public new Timestamp Timestamp { get; set; }
-        public new string Topic { get; set; }
-        public new TopicPartition TopicPartition { get; set; }
+        public new Headers Headers {
+            get => base.Headers;
+            set => base.Headers = value;
+        }
+        public new TKey Key {
+            get => base.Key;
+            set => base.Key = value;
+        }
+        public new Message<TKey, TValue> Message {
+            get => base.Message;
+            set => base.Message = value;
+        }
+        public new Offset Offset {
+            get => base.Offset;
+            set => base.Offset = value;
+        }
+        public new Partition Partition {
+            get => base.Partition;
+            set => base.Partition = value;
+        }
+        public new PersistenceStatus Status {
+            get => base.Status;
+            set => base.Status = value;
+        }
+        public new Timestamp Timestamp {
+            get => base.Timestamp;
+            set => base.Timestamp = value;
+        }
+        public new string Topic {
+            get => base.Topic;
+            set => base.Topic = value;
+        }
+        public new TopicPartition TopicPartition {
+            // base readonly
+            get; set;
+        }
+        public new TopicPartitionOffset TopicPartitionOffset {
+            get => base.TopicPartitionOffset;
+            set => base.TopicPartitionOffset = value;
+        }
     }
 
 }
