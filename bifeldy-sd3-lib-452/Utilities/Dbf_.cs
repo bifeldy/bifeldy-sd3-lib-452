c769eb0c3603fa8f15272770124cea44e0420af4
B. Bias A. Ch <bias@indomaret.co.id>
2024-10-05 10:41:53 +0700

    // TODO :: DBF

---
+++ bifeldy-sd3-lib-452/Utilities/Dbf_.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: D-Base III Files (DBF)
+ *              :: Harap Didaftarkan Ke DI Container
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bifeldy_sd3_lib_452.Utilities {
+
+    public interface IDbf3 { }
+
+    public sealed class CDbf3 : IDbf3 {
+
+        public CDbf3 () {
+            //
+        }
+
+    }
+
+}
